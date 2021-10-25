5f031baf465ff63c3f50de9d6ae45cd1c660ddbb
Manuel Fuchs <manuel.fuchs49@gmail.com>
2021-10-25 11:08:15 +0200

    Add test program outputting test emails

---
+++ Exporter.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test1@tempr.email");
+            Console.WriteLine("test2@tempr.email");
+        }
+    }
+}
